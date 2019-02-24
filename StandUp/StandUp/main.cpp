ffd1867899b90d4f4479f73fc072b8482a22e448
A-Kaga <37213151+A-Kaga@users.noreply.github.com>
2019-02-24 23:00:40 +0800

    Add files via upload

--- StandUp/StandUp/main.cpp
+++ StandUp/StandUp/main.cpp
@@ -2,70 +2,85 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2\highgui\highgui.hpp>
 
+#include <ctime>
+#include <Windows.h>
+
 //#include "FaceRecognition.cpp"
 
 using namespace cv;
 using namespace std;
 
+bool initialization(VideoCapture&, CascadeClassifier&);
+bool get_img(VideoCapture, Mat&);
 bool faceRecognition(Mat, vector<Rect>&, CascadeClassifier);
+void Sit_or_Not(vector<Rect>);
+
+int detected = 0;
+int absent = 0;
 
 int main()
 {
 	Mat frame, frame_gray;
-	VideoCapture cap(0);
+	VideoCapture cam(0);
 	CascadeClassifier face;
 	vector<Rect> faceRect;
 
-	if (!face.load("haarcascade_frontalface_alt.xml"))
-	{
-		cout << "Failed to load haarcascade_frontalface_alt.xml!" << endl;
+	if (!initialization(cam, face))
 		return -1;
-	}
 
-	if (!cap.isOpened())
-	{
-		cout << "Failed to open the camera!" << endl;
-		return -1;
-	}
-
-	namedWindow("BigBrotherVideo", CV_WINDOW_AUTOSIZE);
 	while (1)
 	{
-		if (!cap.read(frame))
-		{
-			cout << "Failed to read the frame" << endl;
+		if (!get_img(cam, frame))
 			break;
-		}
 
 		if (!faceRecognition(frame, faceRect, face))
-		{
-			cout << "Error! Exiting!" << endl;
 			break;
-		}
 
-		/*
-		cvtColor(frame, frame_gray, CV_BGR2GRAY);
-		equalizeHist(frame_gray, frame_gray);
-		face.detectMultiScale(frame_gray, faceRect, 1.1, 2, 0);
-		*/
-
-		for (size_t i = 0; i < faceRect.size(); i++)
-		{
-			rectangle(frame, faceRect[i], Scalar(0, 0, 255), 3);
-		}
+		Sit_or_Not(faceRect);
+		//for (size_t i = 0; i < faceRect.size(); i++)
+		//	rectangle(frame, faceRect[i], Scalar(0, 0, 255), 3);
 
 		imshow("BigBrotherVideo", frame);
 
 		if (waitKey(30) == 27)
-		{
-			cout << "Exiting!" << endl;
 			break;
-		}
+
+		Sleep(10000);
 	}
 
 	waitKey(0);
 	return 0;
 }
+
+
+bool initialization(VideoCapture &cam, CascadeClassifier &face)
+{
+	if (!face.load("haarcascade_frontalface_alt.xml"))
+	{
+		cout << "Failed to load haarcascade_frontalface_alt.xml!" << endl;
+		return false;
+	}
+
+	if (!cam.isOpened())
+	{
+		cout << "Failed to open the camera!" << endl;
+		return false;
+	}
+
+	return true;
+}
+
+
+bool get_img(VideoCapture cam, Mat &frame)
+{
+	if (!cam.read(frame))
+	{
+		cout << "Failed to read the frame" << endl;
+		return false;
+	}
+	return true;
+}
+
 
 bool faceRecognition(Mat img, vector<Rect>& res, CascadeClassifier face)
 {
@@ -76,4 +91,24 @@ bool faceRecognition(Mat img, vector<Rect>& res, CascadeClassifier face)
 	face.detectMultiScale(img_gray, res, 1.1, 2, 0);
 
 	return true;
+}
+
+
+void Sit_or_Not(vector<Rect> faceRect)
+{
+	if (faceRect.size() == 0)
+	{
+		absent++;
+		cout << "Buzai" << endl;
+	}
+	else
+	{
+		absent = 0;
+		detected++;
+		cout << "Zai" << endl;
+	}
+
+	cout << "Current data:"
+		 << "Detected: " << detected << endl
+		 << "Absent: " << absent << endl;
 }
